e9f6453a2e5b5adb610f59eb9f6dec3a14dc1ad4
00edgar <shyzedgar@gmai.com>
2026-03-19 09:42:25 -0600

    feat: :rocket: reportes
    funcion  de reportes

---
+++ funciones.py
@@ -0,0 +1,35 @@
+####################################################################################################
+def separador():
+    print(" "*60)
+#################################################################################################
+def menu_opcion():
+    print("""GENERAR REPORTE
+        1 : para generar reporte dee clientes y su progreso
+        2 : para generaar rorte de servicios, capacidades, e instructotes
+        3 : para salir""")
+#############################################################################################
+def menu_reporte_cleintes():
+    print("""REPORTE CLIENTES 
+         1 : para ver progreso
+         2 : para ver asistencias
+         3 : salir 
+          """)
+def ver_progreso():
+    print ("progreso en espera")
+def ver_asistencia():
+    print ("checando asistencia")
+###############################################################################################
+def menu_reporte_gimnacio():
+   print("""
+         1 : para ver riesgos
+         2 : para ver instructores activos
+         3 : para ver servicios
+         4 : para salir 
+         """)
+def ver_registro():
+    print ("esperando registro")
+def ver_instructores():
+    print("observando monitoires")
+def ver_servicio():
+    print("observando servicios")
+####################################################################################################
